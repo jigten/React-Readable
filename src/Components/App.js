f046dcfd5312d17b221983a1dd15a778b6378b8d
Ugyen Jigten <ujigten@gmail.com>
2017-09-01 01:53:59 +0700

    feat: Add Switch for routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,17 +3,19 @@ import Index from './Index'
 import Category from './Category'
 import Post from './Post'
 import PostNew from './PostNew'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import '../App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route path="/" exact component={Index} />
-        <Route path="/post/new" component={PostNew} />
-        <Route path="/:category" exact component={Category} />
-        <Route path="/:category/:post_id" component={Post} />
+        <Switch>
+          <Route path="/" exact component={Index} />
+          <Route path="/post/new" component={PostNew} />
+          <Route path="/:category" exact component={Category} />
+          <Route path="/:category/:post_id" component={Post} />
+        </Switch>
       </div>
     );
   }
